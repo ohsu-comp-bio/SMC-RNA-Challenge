57201a9f9aa9550910429f6a81292e4d10794f8f
thomasyu888 <thomasyu888@gmail.com>
2016-03-31 15:38:17 -0700

    Seperate validation and evaluation

--- evaluation.py
+++ evaluation.py
@@ -6,16 +6,22 @@ import subprocess
 
 #Add -s
 
-def evaluate(args):
-	val = subprocess.Popen(["python","/opt/SMC-RNA-Challenge/Validator/bedpeValidator.py", "-c", "/opt/SMC-RNA-Challenge/Validator/GRCh37.chromosome.strict.txt", "-i",args.inputbedpe , "-o", "/tmp/output.bedpe"], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+def validate(args):
+	val = subprocess.Popen(["python","/opt/SMC-RNA-Challenge/Validator/bedpeValidator.py", "-c", "/opt/SMC-RNA-Challenge/Validator/GRCh37.chromosome.strict.txt", "-i",args.inputbedpe , "-o", args.outputbedpe], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	out = val.stdout.read()
 	error = val.stderr.read()
-	#if out == '' and error == '':
-	evaluate = subprocess.Popen(["fusionToolEvaluator", "-t", args.truthfile,"-r","/tmp/output.bedpe","-g", "/opt/SMC-RNA-Challenge/examples/ensembl.hg19.txt","-s","/opt/SMC-RNA-Challenge/examples/rulefile.txt","-o",args.outputbedpe], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-	#else:
-		#os.system("touch %s" % args.outputbedpe)
-		#os.system("echo %s > %s" % (out,args.outputbedpe))
-		#os.system("echo %s >> %s" % (error, args.outputbedpe))
+	if out != '' or error != '':
+		with open("error.log",'w') as errors:
+			errors.write("Error!\n")
+			errors.write(out+"\n")
+			errors.write(error)
+			errors.close()
+
+def evaluate(args):
+	if os.path.basename(args.inputbedpe) == "error.log":
+		args.outputbedpe = args.inputbedpe
+	else:
+		evaluate = subprocess.Popen(["fusionToolEvaluator", "-t", args.truthfile,"-r",args.inputbedpe,"-g", "/opt/SMC-RNA-Challenge/examples/ensembl.hg19.txt","-s","/opt/SMC-RNA-Challenge/examples/rulefile.txt","-o",args.outputbedpe], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 
 # ------------------------------------------------------------
 # Args parse
@@ -34,8 +40,8 @@ subparsers = parser.add_subparsers(title='commands',
 		description='The following commands are available:')
 
 #Update beatAML project
-parser_evaluate = subparsers.add_parser('eval',
-		help='validates then evaluates input bedpe')
+parser_evaluate = subparsers.add_parser('evaluate',
+		help='evaluates input bedpe')
 parser_evaluate.add_argument('--inputbedpe',  metavar='fusion.bedpe', type=str, required=True,
 		help='result bedpe'),
 parser_evaluate.add_argument('--outputbedpe', metavar='fusionout.bedpe', type=str, required=True,
@@ -43,6 +49,14 @@ parser_evaluate.add_argument('--outputbedpe', metavar='fusionout.bedpe', type=st
 parser_evaluate.add_argument('--truthfile', metavar='truth.bedpe', type=str, required=True,
 		help='truth file')
 parser_evaluate.set_defaults(func=evaluate)
+
+parser_validate = subparsers.add_parser('validate',
+		help='validates input bedpe')
+parser_validate.add_argument('--inputbedpe',  metavar='fusion.bedpe', type=str, required=True,
+		help='result bedpe'),
+parser_validate.add_argument('--outputbedpe', metavar='fusionout.bedpe', type=str, required=True,
+		help='output bedpe')
+parser_validate.set_defaults(func=validate)
 
 #Parse args
 args = parser.parse_args()
