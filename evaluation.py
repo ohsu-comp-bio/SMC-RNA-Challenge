4386e6265f4fa91fc48c077f442ad13aba1add23
thomasyu888 <thomasyu888@gmail.com>
2016-04-01 00:10:57 -0700

    Add in -s command for validator

--- evaluation.py
+++ evaluation.py
@@ -4,10 +4,8 @@ import os
 import argparse
 import subprocess
 
-#Add -s
-
 def validate(args):
-	val = subprocess.Popen(["python","/opt/SMC-RNA-Challenge/Validator/bedpeValidator.py", "-c", "/opt/SMC-RNA-Challenge/Validator/GRCh37.chromosome.strict.txt", "-i",args.inputbedpe , "-o", args.outputbedpe], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	val = subprocess.Popen(["python","/opt/SMC-RNA-Challenge/Validator/bedpeValidator.py", "-s", "-c", "/opt/SMC-RNA-Challenge/Validator/GRCh37.chromosome.strict.txt", "-i",args.inputbedpe , "-o", args.outputbedpe], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	out = val.stdout.read()
 	error = val.stderr.read()
 	if out != '' or error != '':
@@ -24,12 +22,6 @@ def evaluate(args):
 		evaluate = subprocess.Popen(["fusionToolEvaluator", "-t", args.truthfile,"-r",args.inputbedpe,"-g", "/opt/SMC-RNA-Challenge/examples/ensembl.hg19.txt","-s",args.rulefile,"-o",args.outputbedpe], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 		out = evaluate.stdout.read()
 		error = evaluate.stderr.read()
-		#if error != '':
-	#		with open("error.log",'w') as errors:
-	#			errors.write("Error!\n")
-#				errors.write(out+"\n")
-#				errors.write(error)
-#				errors.close()
 
 # ------------------------------------------------------------
 # Args parse
@@ -41,13 +33,12 @@ def perform_main(args):
 		except Exception as ex:
 			print(ex)
 
-
 parser = argparse.ArgumentParser(description='Evaluate submission')
 
 subparsers = parser.add_subparsers(title='commands',
 		description='The following commands are available:')
 
-#Update beatAML project
+#Evaluate bedpe
 parser_evaluate = subparsers.add_parser('evaluate',
 		help='evaluates input bedpe')
 parser_evaluate.add_argument('--inputbedpe',  metavar='fusion.bedpe', type=str, required=True,
@@ -60,6 +51,7 @@ parser_evaluate.add_argument('--rulefile', metavar='rulefile.txt', type=str, req
 		help='rule file')
 parser_evaluate.set_defaults(func=evaluate)
 
+#Validate bedpe
 parser_validate = subparsers.add_parser('validate',
 		help='validates input bedpe')
 parser_validate.add_argument('--inputbedpe',  metavar='fusion.bedpe', type=str, required=True,
