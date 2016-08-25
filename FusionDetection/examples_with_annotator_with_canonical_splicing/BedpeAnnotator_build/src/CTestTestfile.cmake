5c9ff891ced92ecc5ed59da4a61aae1c8e74fdc0
jin-wash-u <jzhang@genome.wustl.edu>
2016-08-25 12:25:13 -0500

    update1

---
+++ FusionDetection/examples_with_annotator_with_canonical_splicing/BedpeAnnotator_build/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /gscmnt/gc2501/maherlab/jzhang/SMC-RNA/examples_with_annotator/BedpeAnnotator/src
+# Build directory: /gscmnt/gc2501/maherlab/jzhang/SMC-RNA/examples_with_annotator/BedpeAnnotator_build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
