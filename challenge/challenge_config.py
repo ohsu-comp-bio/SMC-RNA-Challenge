1bf3ae7ab0b365a4e52bc97f2225a9cbebcf1490
thomasyu888 <thomasyu888@gmail.com>
2017-03-27 20:06:26 -0700

    If URL ends with a /, remove it

--- challenge/challenge_config.py
+++ challenge/challenge_config.py
@@ -184,6 +184,8 @@ def validate(evaluation,submission,syn,token):
     else:
         assert submission.entity.externalURL.startswith('https://cgc.sbgenomics.com/u'), "Your input URL is not formatted correctly"
         BASE_URL = "https://cgc-api.sbgenomics.com/v2/"
+        if submission.entity.externalURL.endswith("/"):
+            submission.entity.externalURL = submission.entity.externalURL[:-1]
         task = requests.get(BASE_URL + "tasks/" + os.path.basename(submission.entity.externalURL), headers={"X-SBG-Auth-Token" : token} ).json()
         assert task['status'] == 'COMPLETED', "The URL that you put in is invalid"
 
